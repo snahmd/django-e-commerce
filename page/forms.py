bcb27698fd0192fb410b4a379711630cc4461a18
snahmd <sanahmed7@gmail.com>
2019-12-26 23:48:20 +0100

    carousel list added

---
+++ page/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Carousel
+
+
+class CarouselModelForm(forms.ModelForm):
+  class Meta:
+    model = Carousel 
+    fields = [
+      'title',
+      'cover_image',
+    ]
+
--- page/views.py
+++ page/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import Carousel
+from .forms import CarouselModelForm
 
 # Create your views here.
 def index(request):
@@ -8,9 +9,33 @@ def index(request):
   context ['images'] = Carousel.objects.filter(status="published")
   return render(request, 'home/index.html', context)
   
+
+def carousel_list(request):
+  context = dict()
+  context['carousel'] = Carousel.objects.all() 
+  return render(request, 'manage/carousel_list.html', context)
+
+
+def carousel_update(request, pk):
+  context = dict()
+  context['item'] = Carousel.objects.get(pk=pk)
+  return render(request, 'manage/carousel_create.html', context)
+
+
 def carousel_create(request):
+  context = dict()
+  context['form'] = CarouselModelForm()
+  #item = Carousel.objects.first()
+  #context['form'] = CarouselModelForm(instance=item)
   if request.method == "POST":
     print (request.POST)
     print (request.FILES['cover_image'])
+    #carousel = Carousel.objects.create(
+    #  title=request.POST.get('title')
+    #)
+    form = CarouselModelForm(request.POST, request.FILES)
+    print(form)
+    if form.is_valid():
+      form.save()
     messages.success(request, 'Birseyler eklendi ama ne oldu bilemiyorum')
-  return render(request, 'manage/carousel_create.html',{})  
+  return render(request, 'manage/carousel_create.html', context)  
